59c5a2aeb7668f6cad60720af7806aa74af466ac
agkarthik04 <gkarthika04@gmail.com>
2021-04-03 10:20:14 +0530

    dfg

---
+++ eclipse-workspace/AirlineDomain-master/src/org/bank/AxisBank.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class AxisBank {
+	public void deposit() {
+		System.out.println("Interest is 10%");
+	}
+	public void savings() {
+		System.out.println("Savings amount is Rs.50,000");
+	}
+	public void deposit(int amount) {
+		System.out.println("Amounts");
+	}
+}
